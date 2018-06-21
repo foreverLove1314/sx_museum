71c4e47beba78e2c1770350458eff4fc1d63e859
tx <302871586@qq.com>
2018-06-21 19:09:44 +0800

    完成部分效果

--- js/sx_all.js
+++ js/sx_all.js
@@ -3,7 +3,6 @@ $(function () {
         verticalCentered:false,//内容不居中
         afterLoad:function(anchorLink, index){
                 if(index==1){
-
                     $('#first_page').height($(window).height());//高度自适应浏览器窗口高度
                     var li = $('#first_page .left_info .title');
                     for (var i=0;i<=li.length;i++){
@@ -13,9 +12,28 @@ $(function () {
                 }
          }
     });
-    // 环境信息的环境场轮播
-    var envSiteLen=$("#env_site_play ul li").length;
-    $(".env_left_btn").click(function () {
-        $(".env_site_play ul li").eq(0).appendTo($("#env_site_play ul"))
+    //导航栏跳转
+    $(".sx_nav_ul li a").click(function () {
+        $(".sx_nav_ul li a").removeClass("nav_active");//移除其他导航标签的选中样式
+        $(this).addClass("nav_active");//当前标签添加选中样式
+        var nav_item=$(this).parents("li").index();//获取被点击元素父元素的下标
+        console.log(nav_item)
+        //滚动到网站的被选中对应的屏
+        $.fn.fullpage.moveTo((nav_item+1), 0);//
     })
+    // 环境信息的环境场轮播
+    var envSiteLen=$(".env_site_play ul li").length;
+    $(".env_left_btn").click(function () {//环境场向左切换
+        $(".env_site_play ul li").eq(0).appendTo($(".env_site_play ul"));
+    })
+    $(".env_right_btn").click(function () {//环境场向右切换
+        $(".env_site_play ul li").eq(envSiteLen-1).prependTo($(".env_site_play ul"));
+    })
+    $(".env_site_nav a").click(function () {
+        var $moveLi=$(".env_site_play ul li[li-item="+$(this).index()+"]")//获取被点中的元素相对应的场
+        $moveLi.prependTo($(".env_site_play ul"));
+    })
+    //文物走马灯播放效果
+    var show_li=$(".important_relics_show ul li").length;
+    $(".important_relics_show ul").css("width",show_li*(193+20)+"px");
 });
